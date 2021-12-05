4c5bbcb3c0cd24410f853bd59f3d27f28264ee32
Tom Dunlap <tom.a.dunlap@gmail.com>
2021-12-05 12:31:39 -0800

    feat: added formatting for format.cpp.

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <iomanip>
+#include <sstream>
 
 #include "format.h"
 
@@ -8,4 +10,15 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds[[maybe_unused]]) {
+    int h, m, s;
+    std::stringstream ret;
+    h = (seconds / 3600) % 100; // mod 100 to always be two digits
+    m = (seconds / 60) % 60;
+    s = seconds % 60;
+    ret << std::setfill('0') << std::setw(2) << h << ":"
+        << std::setfill('0') << std::setw(2) << m << ":"
+        << std::setfill('0') << std::setw(2) << s; 
+    
+    return ret.str(); 
+}
